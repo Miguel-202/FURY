3030d510d0914e5d86b345eaac71f603ac2d3b90
Miguel-202 <mikytinez@hotmail.com>
2024-01-13 00:10:54 -0500

    Vertex Array Abstraction and OpenGL-Specific Code Refactoring in FURY Game Engine
    ADDED
    - An abstract class for Vertex Arrays to accommodate different APIs, with initial implementation for OpenGL
    - Significantly reduced OpenGL-specific code in the main application by refactoring it into the vertex array management
    
    TODO
    - Modify and relocate the remaining rendering-specific functions to align with the new abstract approach
    - Move the refined OpenGL functionalities to the sandbox application for isolated testing and development
    
    TODO (Recommended)
    - Test the abstract Vertex Array class across different platforms to ensure its functionality with various APIs, despite the current focus on OpenGL
    - Carefully assess the impact of these changes on the engine's overall performance and rendering capabilities, especially in complex rendering scenarios

--- FURY/src/FURY/Application.h
+++ FURY/src/FURY/Application.h
@@ -10,6 +10,7 @@
 
 #include "FURY/Renderer/Shader.h"
 #include "FURY/Renderer/Buffer.h"
+#include "FURY/Renderer/VertexArray.h"
 
 namespace FURY
 {
@@ -40,11 +41,11 @@ namespace FURY
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray;
+		std::shared_ptr<Shader> m_Shader;
+		std::shared_ptr<VertexArray> m_VertexArray;
 
-		std::unique_ptr<Shader> m_Shader;
-		std::unique_ptr<VertexBuffer> m_VertexBuffer;
-		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		std::shared_ptr<Shader> m_BlueShader;
+		std::shared_ptr<VertexArray> m_SquareVA;
 
 		static Application* s_Instance;
 	};
